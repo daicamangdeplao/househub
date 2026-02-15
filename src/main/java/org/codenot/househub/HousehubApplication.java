9920de03cf8bec9a9bf4ff5d05f4b26c26ae4b1c
Trung Dung Nguyen <trungdung.nguyen.000000@gmail.com>
2026-02-15 17:14:00 +0100

    Init

---
+++ src/main/java/org/codenot/househub/HousehubApplication.java
@@ -0,0 +1,13 @@
+package org.codenot.househub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousehubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codenot/househub/HousehubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codenot.househub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
